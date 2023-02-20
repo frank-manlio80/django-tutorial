28bafa89a0fb53ac2ddbaa4762e34a11adeae795
frank-manlio80 <francesco.porcella@itcgtursi.edu.it>
2023-02-20 20:01:38 +0100

    Django Tutorial - parte 3 - Una scorciatoia: render()

--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 
 from .models import Question
 
-
+#Una scorciatoia: render()
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
