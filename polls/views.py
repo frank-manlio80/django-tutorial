3bf0fa8b5ea63372dadf727c97d1084d54ec3e47
frank-manlio80 <francesco.porcella@itcgtursi.edu.it>
2023-02-26 17:13:09 +0100

    Django Tutorial - parte 3 - Una scorciatoia render() ver2

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from django.template import loader
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404
 
 from .models import Question
 
@@ -14,8 +15,8 @@ def index(request):
 
     #return HttpResponse("Hello, world. You're at the polls index.")
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -23,3 +24,11 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
